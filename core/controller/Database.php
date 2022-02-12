67a02d4f353dc5a3f2e53ae33aa258ee99050b7e
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-12 11:01:39 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -7,6 +7,7 @@ class Database {
 	}
 
 	function connect(){
+		$this->user="b04093d684a84f";$this->pass="8f4ac7a8";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_fdf0012e3284d0b";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
