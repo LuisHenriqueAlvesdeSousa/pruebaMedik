ba54344f4398154b343e92fc5bf27b2b06e45972
Luis Henrique Alves de Sousa <luishenrique.alves@ikasle.egibide.org>
2022-02-12 11:24:14 +0100

    reinstalar

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -2,11 +2,29 @@
 class Database {
 	public static $db;
 	public static $con;
-	function Database(){
-		$this->user="b04093d684a84f";$this->pass="8f4ac7a8";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_fdf0012e3284d0b";
+	function Database(){/*
+		if (isset($_ENV["CLEARDB_DATABASE_URL"]))
+		{
+			$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+			$var_server = $cleardb_url["host"];
+			$var_username = $cleardb_url["pass"];
+			$var_db = substr($cleardb_url["path"], 1)
+
+
+		}
+		else //Entorno de desarollo
+		{
+		$var_server = "localhost";
+		$var_username = "username";
+		$var_password = "userpassword";
+		$var_db = "newdb";
+		}*/
+
+		$this->user="b1b673cba6bf71";$this->pass="49259fbe";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_ad3459097eedfff";
 	}
 
 	function connect(){
+		$this->user="b1b673cba6bf71";$this->pass="49259fbe";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_ad3459097eedfff";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
