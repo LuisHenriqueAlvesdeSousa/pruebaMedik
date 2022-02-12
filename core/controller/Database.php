2f3ce1404f397d667c1292203e05aab6ff92e986
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-12 10:32:15 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+		$this->user="b1ddcfb77a5494";$this->pass="bb32d051";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_2ad7720dd8528f8";
 	}
 
 	function connect(){
